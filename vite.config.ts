0e6a2e7a8a1ab93f49374906e1a9279a998d6533
Aadarsh Shrestha <aadarsh.stha36@gmail.com>
2024-08-22 10:45:45 +0545

    test

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react-swc";
+import { default as react, default as swc } from "@vitejs/plugin-react-swc";
 import path from "path";
 import { defineConfig } from "vite";
 import compression from "vite-plugin-compression";
@@ -7,6 +7,7 @@ import compression from "vite-plugin-compression";
 export default defineConfig({
   plugins: [
     react(),
+    swc(),
     compression({
       algorithm: "gzip",
       ext: ".gz",
